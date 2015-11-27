843c2be7091b79f2d6779e0beb2e9974e1e3e38f
igor venegas <venegas.igor@gmail.com>
2015-11-27 20:44:49 -0300

    strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :image, :phone)
+      params.require(:post).permit(:title, :content, :image, :phone, :address ,:longitude, :latitude)
     end
 end
