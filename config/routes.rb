6773978032c956fdd9dcf549b8a61b202ef8a902
igor venegas <venegas.igor@gmail.com>
2015-11-27 19:54:20 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create]
+     resources :comments, only: [:index]
+    end
+    get 'get_address', to: 'locations#get_address'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
