bac7ce628cb875204490e14f480524ab9e602eba
igor venegas <venegas.igor@gmail.com>
2015-11-30 20:04:31 -0300

    git add-all

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'geocoder'
 gem 'kaminari'
 gem 'acts_as_votable'
 gem 'rails_12factor', group: :production
+gem 'omniauth-facebook'  
+gem 'omniauth-twitter'  
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use postgresql as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :authentications, :dependent => :delete_all  
   has_many :posts
   has_many :comments
   devise :database_authenticatable, :registerable,
