ba261496a15a09399bb04001252eb49ce1d92676
igor venegas <venegas.igor@gmail.com>
2015-11-30 21:11:10 -0300

    authentications

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 end
